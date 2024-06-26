ecc8b857f9f8fb2c5a43b49983488c561b7ec106
Carla <dasalcarla812@gmail.com>
2024-06-26 09:26:00 +0800

    first commit

---
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminLoginController extends Controller
+{
+    public function index(){
+        return view('admin.auth.login');
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($request->only('email', 'password'))){
+             if(auth()->user()->is_admin){
+                return redirect()->route('admin.home');
+             }
+             Auth::logout();
+        }
+
+        return back()->withErrors(['email'=>'Wrong Credentials']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Middleware\UserMiddleware;
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    
+    Route::get('/login', [AdminLoginController::class, 'index']);
+    Route::post('/login', [AdminLoginController::class, 'login'])->name('login');
+
+    Route::middleware(['auth', AdminMiddleware::class])->group(function(){
+        Route::get('/home', [AdminHomeController::class, 'index'])->name('home');
+    });
+    
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+->name('home')->middleware(UserMiddleware::class);
